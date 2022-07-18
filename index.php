66850636c6010d7a919556a99e8d3ab08c275143
AfraHanan <afrahanan97@gmail.com>
2022-07-19 04:46:20 +0600

    landing page section hero added

--- index.php
+++ index.php
@@ -3,6 +3,40 @@
 
 <?php  include('includes/header.php')  ?>
 
+        <div class="no-bottom no-top" id="content">
+            <div id="top"></div>
+            <section id="section-hero" aria-label="section" class="no-top no-bottom vh-100" data-bgimage="url(images/background/bg-dark.jpg) bottom">
+                <div class="v-center">
+                    <div class="container">
+                        <div class="row align-items-center">
+                            <div class="col-md-6">
+                                <div class="spacer-single"></div>
+                                <h6 class="wow fadeInUp" data-wow-delay=".5s"><span class="text-uppercase id-color-2">Gamer's Hub</span></h6>
+                                <div class="spacer-10"></div>
+                                <h1 class="wow fadeInUp" data-wow-delay=".75s">Play, Share & Win Real Prizes.</h1>
+                                <p class="wow fadeInUp lead" data-wow-delay="1s">
+                                The ultimate virtual gaming platform for gamers.</p>
+                                <div class="spacer-10"></div>
+                                <a href="dark-explore.html" class="btn-main wow fadeInUp lead" data-wow-delay="1.25s">Explore</a>
+                                <div class="mb-sm-30"></div>
+                            </div>
+                            <div class="col-md-6 xs-hide">
+                                <img src="images/misc/gaming-vector-1.png" class="lazy img-fluid wow fadeIn" data-wow-delay="1.25s" alt="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section> 
+            
+           
+
+        </div>
+        
+        <?php  include('includes/footer.php')  ?>
+
+    </div>
+    
+    <?php  include('includes/scripts.php')  ?>
     
 </body>
 
