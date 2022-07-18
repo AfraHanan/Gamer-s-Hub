97ffddb0df60044b710389c2c03215d287435ff6
AfraHanan <afrahanan97@gmail.com>
2022-07-19 02:50:16 +0600

    header and navigation added

---
+++ includes/header.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>GH - Gamers Hub</title>
+    <link rel="icon" href="images/icon.png" type="image/gif" sizes="16x16">
+    <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+    <meta content="CBW - Cyber Gaming World" name="description" />
+    <meta content="" name="keywords" />
+    <meta content="" name="author" />
+    <!-- CSS Files -->
+    <link id="bootstrap" href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link id="bootstrap-grid" href="css/bootstrap-grid.min.css" rel="stylesheet" type="text/css" />
+    <link id="bootstrap-reboot" href="css/bootstrap-reboot.min.css" rel="stylesheet" type="text/css" />
+    <link href="css/animate.css" rel="stylesheet" type="text/css" />
+    <link href="css/owl.carousel.css" rel="stylesheet" type="text/css" />
+    <link href="css/owl.theme.css" rel="stylesheet" type="text/css" />
+    <link href="css/owl.transitions.css" rel="stylesheet" type="text/css" />
+    <link href="css/magnific-popup.css" rel="stylesheet" type="text/css" />
+    <link href="css/jquery.countdown.css" rel="stylesheet" type="text/css" />
+    <link href="css/mdbstyle.css" rel="stylesheet" type="text/css" />
+    <link id="colors" href="css/scheme-main.css" rel="stylesheet" type="text/css" />
+    <link href="css/coloring.css" rel="stylesheet" type="text/css" />
+    <link rel="import" href="http://stackoverflow.com">
+
+</head>
+
+<body class="dark-scheme">
+    <div id="wrapper">
+        <header class="transparent">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="de-flex sm-pt10">
+                            <div class="de-flex-col">
+                                <div class="de-flex-col">
+                                    <div id="logo">
+                                        <a href="index.php">
+                                            <img alt="" src="#" />
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="de-flex-col header-col-mid">
+
+                                <ul id="mainmenu">
+                                    <li>
+                                        <a href="index.php">Home<span></span></a>
+                                    </li>
+                                    <li>
+                                        <a href="tournaments.php">Tournaments<span></span></a>
+                                    </li>
+
+                                    <li>
+                                        <a href="#">Game Updates<span></span></a>
+                                    </li>
+                                    <li>
+                                        <a href="#">About<span></span></a>
+                                    </li>
+                                    <li>
+                                        <a href="Contact.php">Contact<span></span></a>
+                                    </li>
+                                </ul>
+
+                                <div class="menu_side_area">
+                                <a href="#" class="btn-main">Members</a>
+                                <span id="menu-btn"></span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+    </div>
