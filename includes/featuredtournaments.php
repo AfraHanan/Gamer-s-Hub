f3a46dfbc12512942af186a5273e15aa2cb5e4c9
AfraHanan <afrahanan97@gmail.com>
2022-07-19 08:42:56 +0600

    feature tournament section added and two new page added

---
+++ includes/featuredtournaments.php
@@ -0,0 +1,46 @@
+<section id="section-items" class="no-bottom">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="text-center">
+                    <h2>Latest Tournaments</h2>
+                    <div class="small-border bg-color-2"></div>
+                </div>
+            </div>
+
+                    <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+                        <div class="nft__item">
+                            <div class="author_list_pp">
+                                <a href="">                                    
+                                    <img class="lazy" src="images/author/author-1.jpg" alt="">
+                                    <i class="fa fa-check"></i>
+                                </a>
+                            </div>
+                            <div class="nft__item_wrap">
+                                <a href="">
+                                    <img src="images/background/n1.jpg" >; 
+                                </a>
+                            </div>
+                            <div class="nft__item_info">
+                                <a href="">
+                                    <h4>Title</h4>
+                                </a>
+                                <div class="nft__item_price">
+                                    Player Capacity<span>12</span>
+                                </div>
+                                <div class="nft__item_action">
+                                    <a href="#">Enter Tournament</a>
+                                </div>
+                                <div class="nft__item_like">
+                                    <i class="fa fa-money"></i><span>FREE</span>
+                                </div>                                 
+                            </div> 
+                        </div>
+                
+                    </div>
+
+            
+
+        </div>
+    </div>
+</section>
