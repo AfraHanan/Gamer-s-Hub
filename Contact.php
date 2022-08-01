277e475a44c36c3249fb11798b8b9a43c2f59879
AfraHanan <afrahanan97@gmail.com>
2022-08-02 00:14:16 +0600

    contact us page contact form added

--- Contact.php
+++ Contact.php
@@ -3,10 +3,6 @@
 
 <?php  include('includes/header.php')  ?>
 
-
-
-
-
 <section id="subheader" class="text-light" data-bgimage="url(images/background/subheader-dark.jpg) top">
     <div class="center-y relative text-center">
         <div class="container">
@@ -22,7 +18,45 @@
     </div>
 </section>
 
+<section aria-label="section">
+                <div class="container">
+						<div class="row">
+							
+							<div class="col-lg-8 mb-sm-30">
+							<h3>Do you have any question?</h3>
+							
+							<form name="contactForm" id="contact_form" class="form-border" method="post" action="email.php">
+								<div class="field-set">
+									<input type="text" name="name" id="name" class="form-control" placeholder="Your Name" />
+								</div>
 
+								<div class="field-set">
+									<input type="text" name="email" id="email" class="form-control" placeholder="Your Email" />
+								</div>
+
+								<div class="field-set">
+									<input type="text" name="phone" id="phone" class="form-control" placeholder="Your Phone" />
+								</div>
+
+								<div class="field-set">
+									<textarea name="message" id="message" class="form-control" placeholder="Your Message"></textarea>
+								</div>
+
+								<div class="spacer-half"></div>
+
+								<div id="submit">
+									<input type="submit" id="send_message" value="Submit Form" class="btn btn-main" />
+								</div>
+								<div id="mail_success" class="success">Your message has been sent successfully.</div>
+								<div id="mail_fail" class="error">Sorry, error occured this time sending your message.</div>
+							</form>
+						</div>
+						
+							
+						</div>
+					</div>
+
+            </section>
 
 
 
