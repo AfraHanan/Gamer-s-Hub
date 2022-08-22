0a57abb902bb2d1346f99db98257ccfe53b6b5f4
Afra Hanan <afrahanan97@gmail.com>
2022-08-23 02:15:26 +0600

    gamers panel added

---
+++ gamers/includes/navbar.php
@@ -0,0 +1,98 @@
+    <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+        <div class="sidebar-brand-icon">
+          <i class="fas fa-user-tie"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">Hi, Gamer <sup></sup></div>
+      </a>
+
+      <hr class="sidebar-divider my-0">
+
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">
+          <i class="fas fa-fw fa-tachometer-alt"></i>
+          <span>Dashboard</span></a>
+      </li>
+
+      <hr class="sidebar-divider">
+
+      <div class="sidebar-heading">
+        Core Interfaces
+      </div>
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+          <i class="fas fa-tasks"></i>
+          <span>Tournaments</span>
+        </a>
+        <div id="collapseOne" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Access Tournaments</h6>
+            <a class="collapse-item" href="add_tournaments.php">Add Tournaments</a>
+            <a class="collapse-item" href="view_tournaments.php">View Tournaments</a>
+          </div>
+        </div>
+      </li>
+
+
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
+          <i class="fas fa-swatchbook"></i>
+          <span>Posts</span>
+        </a>
+        <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Write Your Thoughts</h6>
+            <a class="collapse-item" href="">Add New Post</a>
+            <a class="collapse-item" href="">View Posts</a>
+            
+          </div>
+        </div>
+      </li>
+
+      <hr class="sidebar-divider">
+
+      <div class="sidebar-heading">
+        Portals
+      </div>
+
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
+          <i class="fas fa-fw fa-folder"></i>
+          <span>Resources</span>
+        </a>
+        <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">From Admins</h6>
+            <a class="collapse-item" href="#">Gaming Guides</a>
+            <a class="collapse-item" href="#">Tokens</a>
+
+          </div>
+        </div>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="memberslist.php">
+          <i class="fas fa-user"></i>
+          <span>My Profile</span></a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="feedbacks.php">
+          <i class="fas fa-question-circle"></i>
+          <span>Give Feedbacks</span></a>
+      </li>
+
+      <hr class="sidebar-divider d-none d-md-block">
+
+      <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+      </div>
+
+    </ul>
+
+    <a class="scroll-to-top rounded" href="#page-top">
+      <i class="fas fa-angle-up"></i>
+    </a>
